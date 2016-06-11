550a8b75a9a2c7683ba7cafb826d57f7a3e93ff8
Charlie Jackson <nightbeacons@gmail.com>
2016-06-11 17:48:56 -0400

    Initial commit

---
+++ themes/fcr/templates/system/page.tpl.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+
+<title>The Flat-Coated Retriever Society of Canada</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="ddaccordion.js">
+</script>
+
+<link rel="stylesheet" href="fcrsoc.css" type="text/css" />
+<link rel="shortcut icon" href="images/favicon.ico" />
+
+</head>
+
+<body>
+<div id="wrapper">
+
+	<div id="menu">
+	<script type="text/javascript">
+
+
+ddaccordion.init({ //top level headers initialization
+	headerclass: "expandable", //Shared CSS class name of headers group that are expandable
+	contentclass: "categoryitems", //Shared CSS class name of contents group
+	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
+	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
+	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc]. [] denotes no content
+	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+	animatedefault: false, //Should contents open by default be animated into view?
+	persiststate: true, //persist state of opened contents within browser session?
+	toggleclass: ["", "openheader"], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+	togglehtml: ["prefix", "", ""], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
+		//do nothing
+	}
+})
+
+ddaccordion.init({ //2nd level headers initialization
+	headerclass: "subexpandable", //Shared CSS class name of sub headers group that are expandable
+	contentclass: "subcategoryitems", //Shared CSS class name of sub contents group
+	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click" or "mouseover
+	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
+	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc]. [] denotes no content
+	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+	animatedefault: false, //Should contents open by default be animated into view?
+	persiststate: true, //persist state of opened contents within browser session?
+	toggleclass: ["opensubheader", "closedsubheader"], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+	togglehtml: ["none", "", ""], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
+		//do nothing
+	}
+})
+
+ddaccordion.init({ //3rd level headers initialization
+	headerclass: "subsubexpandable", //Shared CSS class name of sub headers group that are expandable
+	contentclass: "subsubcategoryitems", //Shared CSS class name of sub contents group
+	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click" or "mouseover
+	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
+	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
+	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc]. [] denotes no content
+	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
+	animatedefault: false, //Should contents open by default be animated into view?
+	persiststate: true, //persist state of opened contents within browser session?
+	toggleclass: ["opensubheader", "closedsubheader"], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
+	togglehtml: ["none", "", ""], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
+	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
+	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
+		//do nothing
+	}
+})
+
+</script>
+		<img src="images/FCRSC_logo_2012-350x196.jpg"  alt="Flat-Coated Retriever Society of Canada" width="300" height="168" />
+		<br /><br /><br />
+		<div class="arrowlistmenu">
+		<h3 class="menuheader"><a href="index.shtml">Home</a></h3>
+
+		<h3 class="menuheader expandable">Breed Standard</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="breedinfo_origin.shtml">Origin &amp; Purpose</a></li>
+				<li><a href="breedinfo_genapp.shtml">General Appearance</a></li>
+				<li><a href="breedinfo_temper.shtml">Temperment, Size &amp; Gait</a></li>
+				<li><a href="breedinfo_coat.shtml">Coat, Colour &amp; Trimming</a></li>
+				<li><a href="breedinfo_head.shtml">Head</a></li>
+				<li><a href="breedinfo_neck.shtml">Neck &amp; Forequarters</a></li>
+				<li><a href="breedinfo_body.shtml">Body</a></li>
+				<li><a href="breedinfo_hind.shtml">Hindquarters &amp; Tail</a></li>
+			</ul>
+		
+		<h3 class="menuheader expandable">Membership</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="membership_info.shtml">Information</a></li>
+				<li><a href="membership_coe.shtml">Code of Ethics</a></li>
+				<li><a href="membership_onlineform.php">Application/Renewal</a></li>
+				<li><a href="membership_bod.shtml">Board of Directors</a></li>
+				<li><a href="membership_flatout.shtml">Flat-Out Newsletter</a></li>
+				
+			</ul>
+		
+		<h3 class="menuheader expandable">Social and Information</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="social_facebook.shtml">Facebook</a></li>
+				<li><a href="social_healthinfo.shtml">Health Information</a></li>
+			</ul>
+		
+		<h3 class="menuheader expandable">FCRSC Specialty</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="http://specialty2015.flatcoat.ca" target="_blank">2015 - Halifax, NS</a></li>
+				<li><a href="http://www.fcrsc2016specialty.com" target="blank">2016 - Surrey, BC</a></li>
+				
+				<li><a href="" class="subexpandable"><span class="lm-down-arrow"></span>Past Specialties </a>
+					<ul class="subcategoryitems" style="margin-left:5px">
+						<li><a href="http://www.fcrsc2014specialty.com">2014 Kamloops, BC</a></li>
+						<li><a href="http://www.ncfcrc.com/">2013 Brockville, ON</a></li>
+						<li><a href="http://specialty2012.flatcoat.ca/index.shtml">2012 Vancouver, BC</a></li>
+						<li><a href="http://specialty2011.flatcoat.ca/2011_results.shtml" target="_blank">2011 Halifax, NS</a></li>
+						<li><a href="http://specialty2010.flatcoat.ca/2010_results.html" target="_blank">2010 Calgary, AB</a></li>
+						<li><a href="http://specialty2009.flatcoat.ca/2009specialtynew.htm" target="_blank">2009 Van. Isl, BC</a></li>
+						<li><a href="http://specialty2008.flatcoat.ca/2008specialty.htm" target="_blank">2008 Milton, ON</a></li>
+						<li><a href="http://specialty2006.flatcoat.ca/logist06.html" target="_blank">2006 Lethbridge, AB</a></li>
+						<li><a href="http://specialty2005.flatcoat.ca/index.html" target="_blank">2005 Thunder Bay, ON</a></li>
+					</ul>
+				</li>
+			</ul>
+		
+		<h3 class="menuheader expandable">Boosters &amp; Events</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="boosters_bc.shtml">BC</a></li>		
+				<li><a href="boosters_ab.shtml">Alberta/Saskatchewan</a></li>
+				<li><a href="boosters_on.shtml">Ontario</a></li>
+				<li><a href="boosters_qc.shtml">Quebec</a></li>
+				<li><a href="boosters_maritimes.shtml">Maritimes</a></li>
+			</ul>
+		
+		<h3 class="menuheader expandable">Top Dogs</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="" class="subexpandable"><span class="lm-down-arrow"></span>Specialty Winners</a>
+					<ul class="subcategoryitems" style="margin-left: 5px">
+						<li><a href="topdog_special_bob.shtml">Best of Breed</a></li>
+						<li><a href="topdog_special_boo.shtml">Best of Opposite</a></li>
+						<li><a href="topdog_special_winmale.shtml">Winners Male</a></li>
+						<li><a href="topdog_special_winfemale.shtml">Winners Female</a></li>
+						<li><a href="topdog_special_bpb.shtml">Best Puppy in Breed</a></li>
+						<li><a href="topdog_special_hit.shtml">High in Trial</a></li>
+					</ul>
+				</li>
+				<li><a href="topdog_show.shtml">Top Show Flat-Coats</a></li>
+				<li><a href="topdog_obedience.shtml">Top Obedience Flat-Coats</a></li>
+				<li><a href="topdog_agility.shtml">Top Agility Flat-Coats</a></li>
+			</ul>
+
+		<h3 class="menuheader expandable">FCRSC Awards</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="fcrscawards_info.shtml">Information</a></li>
+				<li><a href="fcrscawards_form.shtml">Forms</a></li>
+				<li><a href="fcrscawards_winners.shtml">Winners</a></li>
+			</ul>		
+
+		<h3 class="menuheader expandable">Contact</h3>
+			<ul class="categoryitems" style="margin-left:15px">
+				<li><a href="contact.shtml">General Contacts</a></li>
+				<li><a href="contact_bod.shtml">Board &amp; Regional Contacts</a></li>
+			</ul>
+
+		<br /><br />
+		
+		</div>
+
+	</div>
+	
+	<div id="content">
+	<font class="head">BREED INFO</font>
+	<br /> <br />
+	<font class="subhead">General Appearance</font>
+	<br /><br />
+	The Flat-Coated Retriever combines substance and strength with elegance and refinement, which together with a happy and active demeanour, intelligent expression, and clean lines, have been eloquently described as power without lumber, and raciness without weediness.
+	<br /><br />
+	The distinctive features of the Flat-Coat are the silhouette (moving as well as standing): smooth, effortless movement, head type, coat and character. A proud carriage, responsive attitude, waving tail, and over-all look of quality, strength, style and symmetry complete the picture of the typical Flat-Coat. The Flat-Coat is a strong and elegant working retriever. Quality of structure, balance and harmony of all parts both standing and in motion are essential. As a breed whose purpose is of a utilitarian nature, structure and condition should give every indication of being suited for hard work.
+	<br /><br />
+	In profile the Flat-Coat has a long, clean but substantial head, which is unique to the breed. It is free from eaggerations of stop or cheek and set upon a moderately long neck which flows smoothly into the well-laid-back shoulders. A level topline combined with a deep, long rib cage tapering to a moderate tuck-up create the impression of a blunted triangle. The brisket is well-developed and the forechest forms a prominent prow. The Flat-Coat is well proportioned, strong but elegant: never lacking in length of body or length of leg. The coat is thick and flat lying , and the legs and tail are well feathered. 
+	
+	<br /><br /><br /><br />
+	
+	<div id="footer" class="inpagelink">
+	&copy; FCRSC 2016 &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;  <a class="inpagelink" href="sitemap.html">Sitemap</a>  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70653713-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+	</div>
+	
+	</div>
+	
+	<div id="info">
+	<img src="images/special2015.jpg" width="100" height="100" alt="2015 National Specialty" /> <br /> <a class="inpagelink" href="http://specialty2015.flatcoat.ca">FCRSC National Specialty 2015</a>
+<br /><br />
+<a class="inpagelink" href="www.fcrsc2016specialty.com">2016 FCRSC National Specialty</a>
+<br /><br />
+<img src="images/pawprint2.jpeg" width="15" height="15" alt="New Item" /><a class="inpagelink" href="boosters_ab.shtml">New Booster scheduled for Saskatchewan</a>
+<br /><br />
+<a class="inpagelink" href="boosters_ab_results.shtml#jan2015">Results for Alberta Booster held January 2015</a>
+<br /><br />
+<a class="inpagelink" href="boosters_bc_results.shtml#nov0214">Results for Tracking Test in BC</a> (Updated 2015-01-05)
+<br /><br />
+<a class="inpagelink" href="fcrscawards_winners.shtml">2014 FCRSC Award Winners</a>
+<br /><br />
+
+
+
+
+
+	</div>
+
+</div>
+</body>
+</html>
